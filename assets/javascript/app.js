cbd46667e84ced7b17c72f21ec8887edd33aa84d
Andrew Driban <dribaan@habasitusa.net>
2018-06-13 09:18:04 -0400

    no errors, but not working correctly, loop running through before timer works

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -2,99 +2,99 @@
 var trivia = {
 
     //define questions sub-object
-    q: {
-        q0: "question 0",
-        q1: "question 1",
-        q2: "question 2",
-        q3: "question 3",
-        q4: "question 4",
-        q5: "question 5",
-        q6: "question 6",
-        q7: "question 7",
-        q8: "question 8",
-        q9: "question 9",
-    },
+    q: [
+        "question 0",
+        "question 1",
+        "question 2",
+        "question 3",
+        "question 4",
+        "question 5",
+        "question 6",
+        "question 7",
+        "question 8",
+        "question 9",
+    ],
 
     //define answers sub-object
-    a: {
-        a0: "true",
-        a1: "false",
-        a2: "true",
-        a3: "false",
-        a4: "true",
-        a5: "false",
-        a6: "true",
-        a7: "false",
-        a8: "true",
-        a9: "false",
-    },
+    a: [
+        "true",
+        "false",
+        "true",
+        "false",
+        "true",
+        "false",
+        "true",
+        "false",
+        "true",
+        "false",
+    ],
 
     //define messages sub-object
-    messages: {
-        ram: "Correct!  Prepare for next question",
-        wam: "Incorrect idiot.  Prepare for next question",
-        tom: "Time is up, too slow dork.  Prepare for next question",
-        fam: "Game over, see results below.  Would you like to play again?",
-    },
+    messages: [
+        "Correct!  Prepare for next question",
+        "Incorrect idiot.  Prepare for next question",
+        "Time is up, too slow dork.  Prepare for next question",
+        "Game over, see results below.  Would you like to play again?",
+    ],
 
     //Method displays appropriate message when user runs out of time
     timeUp: function() {
-        $("#m").html(this.messages.tom);
+        $("#m").html(this.messages[2]);
     },
 
     //define gameplay method
     gameplay: function() {
 
         //loop through all questions in question object
-        for (i = 0; i < q.length; i++) {
+        for (var i = 0; i < this.q.length; i++) {
 
             //adding each question to questions div in HTML
-            $("#q").html(this.q["q" + i]);
+            $("#q").html(this.q[i]);
 
             //create true and false buttons for user to select answer
             $("#a").html("<button class=true>True</button> <button class=false>False</button>");
 
             //start timer for thirty seconds after question is printed on screen; calls timeUp method if time runs out
-            setTimeOut(function() { this.timeUp() }, 1000 * 30);
+            setTimeout(function() { this.timeUp() }, 1000 * 30);
 
             //if statements to check if user selected true or false and whether or not chosen answer is correct
-            if ($("true").click()) {
+            if ($(".true").click()) {
 
                 //printing right answer message (ram) if correct answer chosen
                 if (this.a[i] === "true") {
-                    $("#m").html(this.messages.ram);
+                    $("#m").html(this.messages[0]);
 
                     //appending final answer message prompting user to restart the game
-                    if (i === q.length -1) {
-                        $("#m").append(this.messages.fam);
+                    if (i === q.length - 1) {
+                        $("#m").append(this.messages[3]);
                     }
                 }
 
                 //printing wrong answer message (wam) if wrong answer chosen
                 else {
-                    $("#m").html(this.messages.wam);
+                    $("#m").html(this.messages[1]);
 
                     if (i === q.length -1) {
-                        $("#m").append(this.messages.fam);
+                        $("#m").append(this.messages[3]);
                     }
                 }
             }
 
-            else if ($("false").click()) {
+            else if ($(".false").click()) {
 
                 if (this.a[i] === "false") {
-                    $("#m").html(this.messages.ram); 
+                    $("#m").html(this.messages[0]); 
                     
                     if (i === q.length -1) {
-                        $("#m").append(this.messages.fam);
+                        $("#m").append(this.messages[3]);
                     }
                 }
 
                 else {
-                    $("#m").html(this.messages.wam);
+                    $("#m").html(this.messages[1]);
 
                     if (i === q.length -1) {
-                        $("#m").append(this.messages.fam);
+                        $("#m").append(this.messages[3]);
                     }
                 }
             }
