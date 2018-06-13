75a943884732e84f41c5b29741cdf1d0e2088a41
Andrew Driban <dribaan@habasitusa.net>
2018-06-13 12:18:03 -0400

    just messing everything up now, still pulling question1 instead of question0 and not iterating, also if statement to verify no answer populates immediately

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -37,62 +37,68 @@ var trivia = {
         "Game over, see results below.  Would you like to play again?",
     ],
 
+    gamelogic: function(i) {
+
+        //create empty variable to hold event target
+        var eti;
+
+        //adding each question to questions div in HTML
+        $("#q").html(trivia.q[i]);
+
+        //create true and false buttons for user to select answer
+        $("#a").html("<button id=true>True</button> <button id=false>False</button>");
+
+        //if statements to check if user selected true or false and whether or not chosen answer is correct
+        $(document).on("click", "button", function() {
+
+            //printing right answer message (ram) if correct answer chosen
+            eti = event.target.id;
+            if (trivia.a[i] === eti) {
+                $("#m").html(trivia.messages[0]);
+
+                //appending final answer message prompting user to restart the game, clearing interval to stop game from running again once all questions have been asked
+                if (i === trivia.q.length) {
+                    $("#m").append(trivia.messages[3]);
+                    clearInterval();
+                }
+            }
+
+            //printing wrong answer message (wam) if incorrect answer chosen
+            else if (!(trivia.a[i] === eti) && eti) {
+                $("#m").html(trivia.messages[1]); 
+                
+                if (i === trivia.q.length) {
+                    $("#m").append(trivia.messages[3]);
+                    clearInterval();
+                }
+            }
+        })
+
+            //printing times out message (tom) if no answer chosen
+        if (!eti) {
+            $("#m").html(trivia.messages[2]);
+
+            if (i === trivia.q.length) {
+                $("#m").append(trivia.messages[3]);
+                clearInterval();
+            }
+        }
+
+        //iterate counter to move onto next question/answer pair
+        i++;
+    },
+
     //define gameplay method
     gameplay: function() {
+
+        //run game for first question before timer starts
+        trivia.gamelogic(0);
 
         //create counter equal to question and answer number
         var i = 1;
 
-        //initial first question and buttons
-        $("#q").html(trivia.q[0]);
-        $("#a").html("<button id=true>True</button> <button id=false>False</button>")
-
         //set interval for game to run on
-        setInterval( function() {
-
-            //adding each question to questions div in HTML
-            $("#q").html(trivia.q[i]);
-
-            //create true and false buttons for user to select answer
-            $("#a").html("<button id=true>True</button> <button id=false>False</button>");
-
-            //if statements to check if user selected true or false and whether or not chosen answer is correct
-            $(document).on("click", "button", function() {
-
-                //printing right answer message (ram) if correct answer chosen
-                if (trivia.a[i] === event.target.id) {
-                    $("#m").html(trivia.messages[0]);
-
-                    //appending final answer message prompting user to restart the game, clearing interval to stop game from running again once all questions have been asked
-                    if (i === trivia.q.length) {
-                        $("#m").append(trivia.messages[3]);
-                        clearInterval();
-                    }
-                }
-
-                else if (!(trivia.a[i] === event.target.id) && event.target.id) {
-                    $("#m").html(trivia.messages[1]); 
-                    
-                    if (i === trivia.q.length) {
-                        $("#m").append(trivia.messages[3]);
-                        clearInterval();
-                    }
-                }
-
-                else if (!event.target.id) {
-                    $("#m").html(trivia.messages[2]);
-
-                    if (i === trivia.q.length) {
-                        $("#m").append(trivia.messages[3]);
-                        clearInterval();
-                    }
-                }
-            })
-            
-            //iterate counter to move onto next question/answer pair
-            i++;
-
-        }, 30000)
+        setInterval( trivia.gamelogic(i), 3000)
     }
 }
 
