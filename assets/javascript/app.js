8edf7fd97450fdbe936673dadaf294bd885ebf2b
Andrew Driban <dribaan@habasitusa.net>
2018-06-13 10:01:04 -0400

    setInterval works other than the first iteration of the game, need to fix this

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -37,68 +37,80 @@ var trivia = {
         "Game over, see results below.  Would you like to play again?",
     ],
 
-    //Method displays appropriate message when user runs out of time
-    timeUp: function() {
-        $("#m").html(this.messages[2]);
-    },
-
     //define gameplay method
     gameplay: function() {
 
-        //loop through all questions in question object
-        for (var i = 0; i < this.q.length; i++) {
+        //create counter equal to question and answer number
+        var i = 0;
+
+        //set interval for game to run on
+        setInterval( function() {
 
             //adding each question to questions div in HTML
-            $("#q").html(this.q[i]);
+            $("#q").html(trivia.q[i]);
 
             //create true and false buttons for user to select answer
             $("#a").html("<button class=true>True</button> <button class=false>False</button>");
-
-            //start timer for thirty seconds after question is printed on screen; calls timeUp method if time runs out
-            setTimeout(function() { trivia.timeUp() }, 1000 * 30);
 
             //if statements to check if user selected true or false and whether or not chosen answer is correct
             if ($(".true").click()) {
 
                 //printing right answer message (ram) if correct answer chosen
-                if (this.a[i] === "true") {
-                    $("#m").html(this.messages[0]);
+                if (trivia.a[i] === "true") {
+                    $("#m").html(trivia.messages[0]);
 
-                    //appending final answer message prompting user to restart the game
-                    if (i === q.length - 1) {
-                        $("#m").append(this.messages[3]);
+                    //appending final answer message prompting user to restart the game, clearing interval to stop game from running again once all questions have been asked
+                    if (i === trivia.q.length - 1) {
+                        $("#m").append(trivia.messages[3]);
+                        clearInterval();
                     }
                 }
 
                 //printing wrong answer message (wam) if wrong answer chosen
                 else {
-                    $("#m").html(this.messages[1]);
+                    $("#m").html(trivia.messages[1]);
 
-                    if (i === q.length -1) {
-                        $("#m").append(this.messages[3]);
+                    if (i === trivia.q.length -1) {
+                        $("#m").append(trivia.messages[3]);
+                        clearInterval();
                     }
                 }
             }
 
             else if ($(".false").click()) {
 
-                if (this.a[i] === "false") {
-                    $("#m").html(this.messages[0]); 
+                if (trivia.a[i] === "false") {
+                    $("#m").html(trivia.messages[0]); 
                     
-                    if (i === q.length -1) {
-                        $("#m").append(this.messages[3]);
+                    if (i === trivia.q.length -1) {
+                        $("#m").append(trivia.messages[3]);
+                        clearInterval();
                     }
                 }
 
                 else {
-                    $("#m").html(this.messages[1]);
+                    $("#m").html(trvia.messages[1]);
 
-                    if (i === q.length -1) {
-                        $("#m").append(this.messages[3]);
+                    if (i === trivia.q.length -1) {
+                        $("#m").append(trivia.messages[3]);
+                        clearInterval();
                     }
                 }
             }
-        }
+
+            else {
+                $("#m").html(trivia.messages[2]);
+
+                if (i === trivia.q.length -1) {
+                    $("#m").append(trivia.messages[3]);
+                    clearInterval();
+                }
+            }
+            
+            //iterate counter to move onto next question/answer pair
+            i++;
+
+        }, 30000)
     }
 }
 
